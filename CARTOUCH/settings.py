41f90604cdd7501e14d859c7c4ab10625272eec8
Max <maximiliengdeb@gmail.com>
2016-12-19 23:24:00 +0100

    ALLOWED HOSTS

--- CARTOUCH/settings.py
+++ CARTOUCH/settings.py
@@ -27,9 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     
-    'maximiliengdb.pythonanywhere.com',
-    'www.maximiliengdb.pythonanywhere.com',
-    '.pythonanywhere.com',
+    "maximiliengdb.pythonanywhere.com",
+    "www.maximiliengdb.pythonanywhere.com",
+    ".pythonanywhere.com",
+    "pythonanywhere.com",
     '.googleapis.com',
     ]
 
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
