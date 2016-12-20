ff910c4f59a9a673f9cb7475d3f9717a9fda1d2a
Max <maximiliengdeb@gmail.com>
2016-12-20 18:42:05 +0100

    La fin

--- CARTOUCH/settings.py
+++ CARTOUCH/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = "/home/maximiliengdb/CARTOUCH/static"
+STATIC_ROOT = "/home/cartouch/CARTOUCH/static"
 
-MEDIA_ROOT = "/home/maximiliengdb/CARTOUCH/static/media"
+MEDIA_ROOT = "/home/cartouch/CARTOUCH/static/media"
 MEDIA_URL = '/media/'
