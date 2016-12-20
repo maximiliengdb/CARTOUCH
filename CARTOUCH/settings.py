8b7ab7d4770d2f33fa7a27b45fbb0d4b4f126193
Max <maximiliengdeb@gmail.com>
2016-12-20 12:23:26 +0100

    MEDIA

--- CARTOUCH/settings.py
+++ CARTOUCH/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(REPOSITORY_ROOT, '/static/')
+STATIC_ROOT = os.path.join(REPOSITORY_ROOT, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, '/media/')
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media')
