54dd63f8407321a317bc369d498d0ecb16e021c3
Max <maximiliengdeb@gmail.com>
2016-12-20 15:12:03 +0100

    test

--- CARTOUCH/urls.py
+++ CARTOUCH/urls.py
@@ -13,8 +13,4 @@ urlpatterns = [
     url(r'^document/(?P<ref>)$', views.document, name='document'),
     url(r'^lesmatieres/', views.lesmatieres, name='documents'),
     url(r'^mdp/', views.mdp, name='mdp'),
-] 
-
-
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
