9ae053ed3d7f65220137277f9631536a375c277d
Max <maximiliengdeb@gmail.com>
2016-12-20 16:14:40 +0100

    test

--- CARTOUCH/settings.py
+++ CARTOUCH/settings.py
@@ -128,5 +128,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = "/home/maximiliengdb/CARTOUCH/static"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = "/home/maximiliengdb/CARTOUCH/static/media"
 MEDIA_URL = '/media/'
